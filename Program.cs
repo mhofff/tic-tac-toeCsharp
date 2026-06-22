fd63d9eb09e7c7d6332277a9884249e2913421f4
Mhofff <lenaaleksandrovic82@gmail.com>
2026-06-22 17:51:07 +0700

    print table logic

--- Program.cs
+++ Program.cs
@@ -12,6 +12,8 @@ namespace Program
       int pickNow = Convert.ToInt32(Console.ReadLine());
       Console.WriteLine(pickNow);
       printTable(slots);
+      //slots[0] = 1;
+      printTable(slots);
       if (pickNow == slots[0] || pickNow == slots[2] || pickNow == slots[6] || pickNow == slots[8] )
       {
         
@@ -22,12 +24,23 @@ namespace Program
     }
     public static void printTable(int[] array)
     {
-      if ()
-      Console.WriteLine($"¹? | ²? | ³?");
-      Console.WriteLine("————————————");
-      Console.WriteLine($"⁴? | ⁵? | ⁶?");
-      Console.WriteLine("————————————");
-      Console.WriteLine($"⁷? | ⁸? | ⁹?");
+      if (array.All(x => x == 0))//masive only use zero
+      {
+        Console.WriteLine("¹? | ²? | ³?");
+        Console.WriteLine("————————————");
+        Console.WriteLine("⁴? | ⁵? | ⁶?");
+        Console.WriteLine("————————————");
+        Console.WriteLine("⁷? | ⁸? | ⁹?");
+      }
+      else if (array.Any(c => c != 0))
+      {
+        Console.WriteLine($"{array[0]} | ²{array[1]} | ³{array[2]}");
+        Console.WriteLine("————————————");
+        Console.WriteLine($"⁴{array[3]} | ⁵{array[4]} | ⁶{array[5]}");
+        Console.WriteLine("————————————");
+        Console.WriteLine($"⁷{array[6]} | ⁸{array[7]} | ⁹{array[8]}");
+      }
+      
     }
   }
 }
